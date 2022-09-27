e0bec16d45958e05791714705eb6b044154e1a2e
Vlad-UA-KR <vlad.fedorchenkog@gmail.com>
2022-09-27 15:16:44 +0300

    change burger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-$(document).ready(function () {
-	$('.header__burger').click(function (event) {
-		$('.header__burger,.header__menu').toggleClass('active')
-		$('body').toggleClass('lock');
-	});
+$(function () {
+
+	$('.header-blok__header').click(function (e) {
+
+		if ($(e.target.closest('div')).hasClass('header__burger')) {
+			$('.header__burger, .header__menu').toggleClass('active');
+			$('body').toggleClass('lock');
+			return
+		};
+
+		if ($(e.target.closest('a')).hasClass('header__link')) {
+			$('.header__burger, .header__menu').removeClass('active');
+			$('body').removeClass('lock');
+			return
+		};
+	})
 });
